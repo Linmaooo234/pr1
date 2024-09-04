4616e137aa258a89bd0af3d98ac39c2399e708d3
“Linmaooo234” <zamirina.sofiya@mail.ru>
2024-09-04 17:16:39 +0300

    Практика 1

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+// let isAlive = confirm("тЫ ЖИВЧИК?");
+// alert (isAlive);
+
+// alert ("Я JavaScript");
+
+// let age = prompt("Сколько лет Ленину?",100);
+// alert (`Ему ${age} лет!`);
+
+let name = prompt("Какое ваше имя?");
+let surname = prompt("Какая ваша фамилия?");
+let fathername = prompt("Какое ваша отчество?");
+let isTrue = confirm (`Все верно? Вы ${name}, ${surname}, ${fathername}`);
+alert (isTrue);
